07c1c667230b4bfea3dccfb03d3b3727d14f42fc
xoxrainie <xoxrainie@gmail.com>
2024-08-06 21:37:34 +0300

    add login_view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+
+from .forms import CustomUserCreationForm, LoginForm
+
+"""Здесь не использую CBV и базовые представления авторизации django,
+             иначе было бы показывать нечего:D"""
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            username = cd["username"]
+            password = cd["password"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+        return redirect("/")
+    if request.method == "GET":
+        form = LoginForm()
+        return render(request, "registration/login.html", context={"form": form})
