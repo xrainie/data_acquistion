6983d28855ad369336975dfca5c501e4bcf39597
xoxrainie <xoxrainie@gmail.com>
2024-08-06 18:55:52 +0300

    fix config and migrate db

--- admin_panel/settings/base.py
+++ admin_panel/settings/base.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.config.AppConfig',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
